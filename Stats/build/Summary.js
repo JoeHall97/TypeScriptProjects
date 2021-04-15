2a0270bfb37c832f7471e81dd1a329a3f8055156
Joseph <beans@localhost.localdomain>
2021-04-15 12:56:21 +1200

    Removed built js files, updated .gitignore to ensure that they're not commited.

--- Stats/build/Summary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Summary = void 0;
-var Summary = /** @class */ (function () {
-    function Summary() {
-    }
-    return Summary;
-}());
-exports.Summary = Summary;
